16e6ad987216aca0b56a12630fafbee2ff2eeb6b
Marius Posta <marius@airbyte.io>
2024-09-25 10:15:40 -0400

    bulk-cdk-toolkits-extract-cdc,source-mysql-v2: flesh out CDC

--- airbyte-cdk/bulk/toolkits/extract-cdc/src/main/kotlin/io/airbyte/cdk/read/cdc/Debezium.kt
+++ airbyte-cdk/bulk/toolkits/extract-cdc/src/main/kotlin/io/airbyte/cdk/read/cdc/Debezium.kt
@@ -6,34 +6,81 @@ package io.airbyte.cdk.read.cdc
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.NullNode
+import io.airbyte.cdk.command.OpaqueStateValue
+import io.airbyte.cdk.read.Stream
+import io.airbyte.protocol.models.v0.AirbyteRecordMessage
+import io.debezium.relational.history.HistoryRecord
+import org.apache.kafka.connect.source.SourceRecord
 
-/** [Record] wraps a Debezium change data event. */
-data class DebeziumRecord(val debeziumEventValue: JsonNode) {
+/** [DebeziumRecord] wraps a Debezium change data event. */
+@JvmInline
+value class DebeziumRecord(val wrapped: JsonNode) {
 
     /** True if this is a Debezium heartbeat event. These aren't passed to [DebeziumConsumer]. */
     val isHeartbeat: Boolean
-        get() = source().isNull
+        get() = source.isNull
 
     /** The datum prior to this event; null for insertions. */
-    fun before(): JsonNode {
-        return element("before")
-    }
+    val before: JsonNode
+        get() = element("before")
 
     /** The datum following this event; null for deletions. */
-    fun after(): JsonNode {
-        return element("after")
-    }
+    val after: JsonNode
+        get() = element("after")
 
     /** Metadata containing transaction IDs, LSNs, etc; null for heartbeats. */
-    fun source(): JsonNode {
-        return element("source")
-    }
+    val source: JsonNode
+        get() = element("source")
 
     /** Convenience function for accessing child object nodes of the debezium event root node. */
-    fun element(fieldName: String?): JsonNode {
-        if (!debeziumEventValue.has(fieldName)) {
+    fun element(fieldName: String): JsonNode {
+        if (!wrapped.has(fieldName)) {
             return NullNode.getInstance()
         }
-        return debeziumEventValue[fieldName]
+        return wrapped[fieldName]
     }
+}
+
+/** [DebeziumOffset] wraps the contents of a Debezium offset file. */
+@JvmInline value class DebeziumOffset(val wrapped: Map<JsonNode, JsonNode>)
+
+/** [DebeziumSchemaHistory] wraps the contents of a Debezium schema history file. */
+@JvmInline value class DebeziumSchemaHistory(val wrapped: List<HistoryRecord>)
+
+/** Debezium Engine input. */
+data class DebeziumInput(
+    val properties: Map<String, String>,
+    val state: DebeziumState,
+    val isSynthetic: Boolean,
+)
+
+/** Debezium Engine output, other than records of course. */
+data class DebeziumState(
+    val offset: DebeziumOffset,
+    val schemaHistory: DebeziumSchemaHistory?,
+)
+
+/** Stateless connector-specific Debezium operations. */
+interface DebeziumOperations<T : Comparable<T>> {
+
+    /** Transforms a [DebeziumRecord] into an [AirbyteRecordMessage]. */
+    fun toAirbyteRecordMessage(record: DebeziumRecord): AirbyteRecordMessage
+
+    /** Extracts the WAL position from a [DebeziumOffset]. */
+    fun position(offset: DebeziumOffset): T
+
+    /** Tries to extract the WAL position from a [DebeziumRecord]. */
+    fun position(record: DebeziumRecord): T?
+
+    /** Tries to extract the WAL position from a [SourceRecord]. */
+    fun position(sourceRecord: SourceRecord): T?
+
+    /** Synthesizes a [DebeziumInput] when no incumbent [OpaqueStateValue] is available. */
+    fun synthesize(): DebeziumInput
+
+    /** Builds a [DebeziumInput] using an incumbent [OpaqueStateValue]. */
+    fun deserialize(opaqueStateValue: OpaqueStateValue, streams: List<Stream>): DebeziumInput
+
+    /** Maps a [DebeziumState] to an [OpaqueStateValue]. */
+    fun serialize(debeziumState: DebeziumState): OpaqueStateValue
 }
